4c8c9265435309983939576698f93beb39edd872
Erick Reyes Lopez <reyeslopezerick@gmail.com>
2025-08-25 16:29:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -406,7 +406,7 @@ export const updateRolePermission = async (role: Role, permissionId: number, has
     }
 }
 
-export async function registerUser(formData: FormData) {
+export async function registerUser(prevState: { error: string } | undefined, formData: FormData) {
     const name = formData.get('name') as string;
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
@@ -435,3 +435,5 @@ export async function registerUser(formData: FormData) {
         return { error: 'An internal error occurred. Please try again.' };
     }
 }
+
+    
